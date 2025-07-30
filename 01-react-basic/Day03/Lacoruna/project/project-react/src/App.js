6db5b142753dfef912d9c4197f1894d4a5c9819c
Lacoruna <jyp0430@naver.com>
2025-07-30 17:27:23 +0900

    feat: Add code for Day03
    - props
    - create-react-app

---
+++ 01-react-basic/Day03/Lacoruna/project/project-react/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.title}>hoya</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
